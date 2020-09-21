f6d15ff03c30e244bc990d955d4fe71caa8ed91f
Thanh Dat <datmathoingu@gmail.com>
2020-09-21 23:08:55 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace xay_dung_class_animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
+        }
+    }
+}
